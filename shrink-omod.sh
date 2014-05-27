be7beca0ec034acd5bdbd315d4acdadb8cf7cbc5
Christian Neumann <x-ian@x-ian.net>
2014-05-27 15:51:06 +0200

    reduced size of omod by stripping big (useless?) stuff from pdfbox

---
+++ shrink-omod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf 1
+mkdir 1
+cd 1
+jar xf ../omod/target/emtfrontend-0.2-SNAPSHOT.omod 
+mkdir ../2
+cd ../2
+jar xf ../1/lib/pdfbox-1.8.5.jar 
+rm -rf org/apache/pdfbox/resources/cmap/
+jar cf ../1/lib/pdfbox-1.8.5.jar .
+cd ../1
+jar cf ../omod/target/emtfrontend-0.2-SNAPSHOT.omod .
+cd ..
+rm -rf 1 2
+
