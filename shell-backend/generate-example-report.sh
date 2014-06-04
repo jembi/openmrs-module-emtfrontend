cc732ccb5990758c8906218d34422485327ab967
Christian Neumann <x-ian@x-ian.net>
2014-06-04 10:21:43 +0200

    added comments and parameter checks

--- shell-backend/generate-example-report.sh
+++ shell-backend/generate-example-report.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./generate-report.sh 20140501 20140531 emt.pdf
+# Convenience script to generate a test report
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$BASEDIR/generate-report.sh 20140501 20140531 emt.pdf
--- shell-backend/generate-report.sh
+++ shell-backend/generate-report.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Generates a report
+
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage example: generate-report.sh 20140501 20140531 emt.pdf"
+    exit 1
+fi
+
 STARTDATE=$1
 ENDDATE=$2
 OUTPUTPDF=$3
